6ab144ee66b7199b0ceb52c16a3fa70c467c819d
brian <bu@brianuy.me>
2026-09-10 16:25:09 +0100

    added basic window and exit input

--- src/main.c
+++ src/main.c
@@ -1,40 +1,99 @@
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, char *argv[])
-{
+// SDL3 includes
+#include <SDL3/SDL.h>
+
+// initialising global variables
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+    // initialise SDL3
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "Error, problem initializing SDL. \n", SDL_GetError());
+        return false;
+    }
+    
+    /*================================================================================*/ // window
+    // creating SDL window, assigning window value
+    window = SDL_CreateWindow(
+        "rasteriser",               // window name
+        800,                        // width
+        600,                        // height
+        SDL_WINDOW_RESIZABLE
+    );
+    // check if window is working
+    if (!window) {
+        fprintf(stderr, "Error, creating SDL window \n", SDL_GetError());
+        return false;
+    }
+    // setting windows position, returns bool in SDL3
+    if (!SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED)) {
+        fprintf(stderr, "Error, setting position SDL window \n", SDL_GetError());
+        return false; 
+    }  
+    SDL_ShowWindow(window);
+    
+    /*================================================================================*/ // renderer
+    // creating SDL renderer
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        fprintf(stderr, "Error, renderer could not be created \n", SDL_GetError());
+        return false;
+    }
+    
+    return true;
+}
+
+void setup(void) {
+
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    
+    switch (event.type) {
+        case SDL_EVENT_QUIT:
+            is_running = false;
+            break;
+        case SDL_EVENT_KEY_DOWN:
+            if (event.key.key == SDLK_ESCAPE)
+                is_running = false;
+            break;
+    }
+
+}
+
+void update(void) {
+    
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 52, 53, 65, 255);          // set screen colour (rgba)
+    SDL_RenderClear(renderer);                                  // clear whole window
+
+
+    SDL_RenderPresent(renderer);
+}
+
+
+int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
-
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("SDL_Init failed: %s", SDL_GetError());
-        return 1;
+    
+    is_running = initialize_window();
+    
+    setup();
+    
+    while (is_running) {
+        process_input();
+        update();
+        render();
     }
-
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
-    if (!SDL_CreateWindowAndRenderer("software-rasteriser", 800, 600, 0, &window, &renderer)) {
-        SDL_Log("SDL_CreateWindowAndRenderer failed: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    bool running = true;
-    while (running) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                running = false;
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 24, 24, 28, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    
+    
     return 0;
 }
